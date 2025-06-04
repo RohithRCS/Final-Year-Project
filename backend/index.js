197ce99beb282c51d4d847069b95a99a27fe78a2
RohithRCS <146531617+RohithRCS@users.noreply.github.com>
2025-06-04 11:16:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app = require('./app') 
 const logger = require('./utils/logger')
+const PORT= process.env.PORT ||1234
 
-app.listen(1234, () => {
-  logger.info(`Server running on port 1234`)
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
